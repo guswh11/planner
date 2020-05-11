dddc36b873cafd0202476b3bf504ef1323699dc2
hyunjin <guswh11@naver.com>
2020-05-11 21:42:01 +0900

    Create SettingFragment and fragment_setting.xml
    Signed-off-by: hyunjin <guswh11@naver.com>

---
+++ app/src/main/java/com/joy/planner/view/settings/SettingFragment.java
@@ -0,0 +1,19 @@
+package com.joy.planner.view.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.joy.planner.R;
+
+public class SettingFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+}
