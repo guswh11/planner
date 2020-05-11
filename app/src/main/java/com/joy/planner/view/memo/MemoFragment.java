29ee2e9b8cd061934041f0651d5a935e5b0d7020
hyunjin <guswh11@naver.com>
2020-05-11 21:40:44 +0900

    Create MemoFragment.java and fragment_memo.xml
    Signed-off-by: hyunjin <guswh11@naver.com>

---
+++ app/src/main/java/com/joy/planner/view/memo/MemoFragment.java
@@ -0,0 +1,19 @@
+package com.joy.planner.view.memo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.joy.planner.R;
+
+public class MemoFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_memo, container, false);
+    }
+}
