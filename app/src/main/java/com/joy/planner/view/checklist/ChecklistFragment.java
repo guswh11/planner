31bb25c41ed8903c0ebfd87f3c42b8c869cc1d30
hyunjin <guswh11@naver.com>
2020-05-11 21:33:11 +0900

    Create ChecklistFragment.java and fragment_checklist.xml
    Signed-off-by: hyunjin <guswh11@naver.com>

---
+++ app/src/main/java/com/joy/planner/view/checklist/ChecklistFragment.java
@@ -0,0 +1,19 @@
+package com.joy.planner.view.checklist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.joy.planner.R;
+
+public class ChecklistFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_checklist, container, false);
+    }
+}
