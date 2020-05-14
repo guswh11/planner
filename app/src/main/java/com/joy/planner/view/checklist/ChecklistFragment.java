d8e60e19e61ec9f88c4146ef9bc227b317a2370d
hyunjin <guswh11@naver.com>
2020-05-14 20:32:01 +0900

    Implement datepicker on ChecklistFragment
    Signed-off-by: hyunjin <guswh11@naver.com>

--- app/src/main/java/com/joy/planner/view/checklist/ChecklistFragment.java
+++ app/src/main/java/com/joy/planner/view/checklist/ChecklistFragment.java
@@ -4,16 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.joy.planner.R;
 
 public class ChecklistFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_checklist, container, false);
+        View view = inflater.inflate(R.layout.fragment_checklist, container, false);
+        Button button = view.findViewById(R.id.button);
+
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText("Select a date");
+        final MaterialDatePicker datePicker = builder.build();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show(getActivity().getSupportFragmentManager(), "date_picker");
+            }
+        });
+
+
+        return view;
     }
 }
