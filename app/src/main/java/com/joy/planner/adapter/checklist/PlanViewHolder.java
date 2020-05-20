2b56511821aac41607221f340f3d6f7f764c8b20
hyunjin <guswh11@naver.com>
2020-05-20 21:10:41 +0900

    Create PlanViewHolder.java
    Signed-off-by: hyunjin <guswh11@naver.com>

---
+++ app/src/main/java/com/joy/planner/adapter/checklist/PlanViewHolder.java
@@ -0,0 +1,12 @@
+package com.joy.planner.adapter.checklist;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlanViewHolder extends RecyclerView.ViewHolder{
+    public PlanViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
