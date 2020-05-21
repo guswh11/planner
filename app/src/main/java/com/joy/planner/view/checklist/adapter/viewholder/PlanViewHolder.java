93e245672e4fb100050bd240b367afd764587326
hyunjin <guswh11@naver.com>
2020-05-21 21:57:31 +0900

    Implement PlanViewHolder
    Signed-off-by: hyunjin <guswh11@naver.com>

--- app/src/main/java/com/joy/planner/view/checklist/adapter/viewholder/PlanViewHolder.java
+++ app/src/main/java/com/joy/planner/view/checklist/adapter/viewholder/PlanViewHolder.java
@@ -1,12 +1,27 @@
 package com.joy.planner.view.checklist.adapter.viewholder;
 
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.joy.planner.R;
+
 public class PlanViewHolder extends RecyclerView.ViewHolder{
+    private TextView planTextView;
+    private ImageButton statusImgBtn;
+    private ImageButton dragImgBtn;
+
     public PlanViewHolder(@NonNull View itemView) {
         super(itemView);
+        planTextView = itemView.findViewById(R.id.text_view_plan);
+        statusImgBtn = itemView.findViewById(R.id.img_btn_status);
+        dragImgBtn = itemView.findViewById(R.id.img_btn_drag);
+    }
+
+    public void setPlan(String plan) {
+        planTextView.setText(plan);
     }
 }
