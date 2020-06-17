1926b5a1ae069e8e6085a6a0b49e59ca57e96332
hyunjin <guswh11@naver.com>
2020-06-17 22:28:49 +0900

    Modify PlanItemView
    Signed-off-by: hyunjin <guswh11@naver.com>

--- app/src/main/java/com/joy/planner/view/checklist/PlanItemView.java
+++ app/src/main/java/com/joy/planner/view/checklist/PlanItemView.java
@@ -1,5 +1,6 @@
 package com.joy.planner.view.checklist;
 
 public interface PlanItemView {
-    void setPlan(String plan);
+    void setPlanName(String plan);
+    void setPlanContent(String plan);
 }
