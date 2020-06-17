2a1e541b986b05a4515fff5111f9fe15a73458a7
hyunjin <guswh11@naver.com>
2020-06-17 22:38:30 +0900

    Implement PlanListPresenter
    Signed-off-by: hyunjin <guswh11@naver.com>

--- app/src/main/java/com/joy/planner/view/checklist/adapter/model/Plan.java
+++ app/src/main/java/com/joy/planner/view/checklist/adapter/model/Plan.java
@@ -1,5 +1,10 @@
 package com.joy.planner.view.checklist.adapter.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Plan {
     private String planName;
     private String planContent;
--- app/src/main/java/com/joy/planner/view/checklist/presenter/PlanListPresenter.java
+++ app/src/main/java/com/joy/planner/view/checklist/presenter/PlanListPresenter.java
@@ -1,5 +1,24 @@
 package com.joy.planner.view.checklist.presenter;
 
-public class PlanListPresenter {
+import com.joy.planner.view.checklist.PlanItemView;
+import com.joy.planner.view.checklist.adapter.model.Plan;
 
+import java.util.List;
+
+public class PlanListPresenter {
+    private final List<Plan> planList;
+
+    public PlanListPresenter(List<Plan> planList) {
+        this.planList = planList;
+    }
+
+    public void onBindPlanItemViewAtPosition(int position, PlanItemView planItemView) {
+        Plan plan = planList.get(position);
+        planItemView.setPlanName(plan.getPlanName());
+        planItemView.setPlanContent(plan.getPlanContent());
+    }
+
+    public int getPlanListSize() {
+        return planList.size();
+    }
 }
