dee7ee3a60f00836cbe54b6cc548db243978dd75
hyunjin <guswh11@naver.com>
2020-06-22 21:12:15 +0900

    Implement planRecyclerView
    Signed-off-by: hyunjin <guswh11@naver.com>

--- app/src/main/java/com/joy/planner/view/checklist/ChecklistFragment.java
+++ app/src/main/java/com/joy/planner/view/checklist/ChecklistFragment.java
@@ -10,10 +10,17 @@ import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.joy.planner.R;
 import com.joy.planner.databinding.FragmentChecklistBinding;
+import com.joy.planner.view.checklist.adapter.PlanRecyclerViewAdapter;
+import com.joy.planner.view.checklist.adapter.model.Plan;
+import com.joy.planner.view.checklist.presenter.PlanListPresenter;
+
+import java.util.ArrayList;
 
 public class ChecklistFragment extends Fragment {
     FragmentManager fragmentManager;
@@ -21,7 +28,6 @@ public class ChecklistFragment extends Fragment {
     MaterialDatePicker datePicker;
 
     AddPlanFragmentDialog addPlanFragmentDialog;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -36,6 +42,22 @@ public class ChecklistFragment extends Fragment {
         MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.datePicker();
         builder.setTitleText("Select a date");
         datePicker = builder.build();
+
+        //create plan-recyclerview
+        ArrayList<Plan> planList = new ArrayList<>();
+        for(int i=0;i<4;i++){
+            Plan plan = new Plan();
+            plan.setPlanName("plan"+i);
+            plan.setPlanContent("content"+i);
+            planList.add(plan);
+        }
+
+        binding.listViewPlan.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+
+        PlanListPresenter presenter = new PlanListPresenter(planList);
+        PlanRecyclerViewAdapter adapter = new PlanRecyclerViewAdapter(presenter);
+        binding.listViewPlan.setAdapter(adapter);
+
 
         return view;
     }
